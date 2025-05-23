a5fe265a6b8d7ab9e163adcdfe9c23356a980b27
Geider Frias <geiderde20@gmail.com>
2025-05-22 23:52:08 -0300

    feat(screen): added viewBiding library

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -4,18 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
-        val signupScreen = findViewById<TextView>(R.id.txtRegistrate)
-        signupScreen.setOnClickListener{
-            val intent = Intent(this, SignupActivity::class.java)
+        binding.txtRegistrate.setOnClickListener {
+            val intent = Intent(this,SignupActivity::class.java)
             startActivity(intent)
         }
+
     }
 
 }
--- app/src/main/java/com/example/kotlin/SignupActivity.kt
+++ app/src/main/java/com/example/kotlin/SignupActivity.kt
@@ -1,11 +1,22 @@
 package com.example.kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlin.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+
+    private  lateinit var binding : ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.txtIngresar2.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

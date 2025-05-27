6659f9d0239fd8b4ae04e78edc1dbbd6021ca1cc
Geider Frias <geiderde20@gmail.com>
2025-05-27 15:27:51 -0300

    feat(vehicle): added style screen list-vehicles

---
+++ app/src/main/java/com/example/kotlin/Entities/Vehiculo.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin.Entities
+
+class Vehiculo{
+    val Id = ""
+    val Matricula: String = ""
+    val Modelo : String = ""
+    val Marca : String = ""
+    val Color : String = ""
+    val Descripcion : String = ""
+    val Imagen : String = ""
+    val Telefono : Int = 0
+}

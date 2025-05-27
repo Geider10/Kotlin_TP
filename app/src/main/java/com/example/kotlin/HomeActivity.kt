631380a57ecabd9183a86ef4e6cd6f993a26cdfd
Geider Frias <geiderde20@gmail.com>
2025-05-27 14:11:35 -0300

    feat(menu-drawer): added navigatin and style menu

--- app/src/main/java/com/example/kotlin/HomeActivity.kt
+++ app/src/main/java/com/example/kotlin/HomeActivity.kt
@@ -2,10 +2,70 @@ package com.example.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.example.kotlin.databinding.ActivityHomeBinding
+import com.example.kotlin.fragments.OneFragment
+import com.example.kotlin.fragments.SecondFragment
+import com.google.android.material.navigation.NavigationView
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        drawerLayout = binding.drawerLayout
+
+        val toolBar = binding.toolBar
+        setSupportActionBar(toolBar)
+
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.nav_drawer_home_open,R.string.nav_drawer_home_close)
+        drawerLayout.addDrawerListener(toggle)
+
+        binding.navigationView.setNavigationItemSelectedListener(this)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu_burger)
+
+        if(savedInstanceState == null){
+            replaceFragment(OneFragment())
+            binding.navigationView.setCheckedItem(R.id.nav_item_one)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_item_one -> {
+                replaceFragment(OneFragment())
+            }
+            R.id.nav_item_two -> {
+                replaceFragment(SecondFragment())
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, fragment)
+        transaction.commit()
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
     }
 }

c32737bbddacb5ddbed18d3c442b7062ee13b757
Geider Frias <geiderde20@gmail.com>
2025-06-09 19:27:09 -0300

    updated adapter of recyclerview

--- app/src/main/java/com/example/kotlin/VehiculoAdapter.kt
+++ app/src/main/java/com/example/kotlin/VehiculoAdapter.kt
@@ -7,12 +7,16 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin.Entities.Vehiculo
+import com.example.kotlin.databinding.CardVehicleBinding
+import com.example.kotlin.databinding.FragmentOneBinding
 
 class VehiculoAdapter (val listVehicles : List<Vehiculo>) : RecyclerView.Adapter<VehiculoAdapter.VehiculoViewHolder>() {
 
+    //inflar el item xml
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VehiculoViewHolder {
-        val layoutInflate = LayoutInflater.from(parent.context).inflate(R.layout.card_vehicle,parent,false)
-        return  VehiculoViewHolder(layoutInflate)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = CardVehicleBinding.inflate(inflater,parent,false)
+        return  VehiculoViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: VehiculoViewHolder, position: Int) {
@@ -24,17 +28,16 @@ class VehiculoAdapter (val listVehicles : List<Vehiculo>) : RecyclerView.Adapter
         return listVehicles.size
     }
 
-    class VehiculoViewHolder (itemView : View) : RecyclerView.ViewHolder(itemView){
-        var registrationVehicle : TextView = itemView.findViewById(R.id.tvMatriculaAuto)
-        var brandVehicle : TextView = itemView.findViewById(R.id.tvMarcaAuto)
-        var modelVehicle : TextView = itemView.findViewById(R.id.tvModeloAuto)
-        var colorVehicle : TextView = itemView.findViewById(R.id.tvColorAuto)
+    class VehiculoViewHolder (private val binding : CardVehicleBinding) : RecyclerView.ViewHolder(binding.root){
 
         fun setValues (vehiculo: Vehiculo){
-            registrationVehicle.text = vehiculo.Matricula
-            brandVehicle.text = vehiculo.Marca
-            modelVehicle.text = vehiculo.Modelo
-            colorVehicle.text = vehiculo.Color
+            with(binding){
+                tvMatriculaAuto.text = vehiculo.Matricula
+                tvMarcaAuto.text = vehiculo.Marca
+                tvModeloAuto.text = vehiculo.Modelo
+                tvColorAuto.text = vehiculo.Color
+            }
+
         }
     }
 }
